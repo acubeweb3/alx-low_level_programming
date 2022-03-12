a9e5a4ca352a0c59d815091ec7d8938eac96209f
acubeweb3 <imranyt41@gmail.com>
2022-03-11 20:19:51 -0800

    This command to push to 0x01

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints a text according number
- *   *
- *    * Return: Always (Success)
- *     */
+* main - Prints a text according number
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
 		int n, lastd;
-		
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 					lastd = n % 10;
