108100bffade65818d3159ac6835c92f41ca6d3f
acubeweb3 <67747350+acubeweb3@users.noreply.github.com>
2022-03-12 05:59:42 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,15 @@
 int main(void)
 {
 	int c;
-
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-
 		if (c != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
