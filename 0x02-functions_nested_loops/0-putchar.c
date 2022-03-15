c3f8edaf253480485a65285a66bd5d608bb230ee
acubeweb3 <67747350+acubeweb3@users.noreply.github.com>
2022-03-15 22:56:46 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: Write a program that prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(c[n]);
+
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
